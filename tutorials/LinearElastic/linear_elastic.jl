a3b4e0bf76c587509eb6826cfb7ab2300b2c34f4
mforets <mforets@gmail.com>
2020-08-21 15:19:31 -0300

    add files

---
+++ tutorials/LinearElastic/linear_elastic.jl
@@ -0,0 +1,8 @@
+# # Linear Elastic Solid
+#
+# In this tutorial we consider a linear elastic solid submitted to uniaxial loading.
+# The geometry given by $L_x$, $L_y$ and $L_z$, and a tension $p$ is applied on
+# the face at $x=L_x$, see the diagram below.
+
+#md # !!! note "TO-DO"
+#md #     Continue the tutorial as in the [User Guide](https://www.fing.edu.uy/~jorgepz/onsas/mainUserGuide.html).
