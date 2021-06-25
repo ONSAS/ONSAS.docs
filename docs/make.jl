7b0bda9ed44dfd42f5053870aa320628deba924f
jorgepz <jorgepz@fing.edu.uy>
2021-06-25 11:22:37 -0300

    start setting dual output html and pdf

--- docs/make.jl
+++ docs/make.jl
@@ -3,38 +3,57 @@ using Documenter, ONSAS_docs
 DocMeta.setdocmeta!(ONSAS_docs, :DocTestSetup,
                     :(using ONSAS_docs); recursive=true)
 
-# Generate bibliography
-include("bibliography.jl")
+# sets default format or read provided argument
+if length(ARGS)==0
+  outputFormat = "html"
+else
+  outputFormat = ARGS[1]
+end
 
-makedocs(
+# Generate bibliography
+#include("bibliography.jl")
+
+
+# sets different format and pages settings for each output format
+if outputFormat == "pdf"
+  makedocs(
     sitename = "ONSAS.m",
     modules = [ONSAS_docs],
-
-    # ---------------------------
-    # case HTML output
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-
-    # ---------------------------
-    # case PDF output
-    #format = Documenter.LaTeX(),
-
-    # ---------------------------
-
+    format = Documenter.LaTeX(),
     pages = [
-        "Home" => "index.md",
-        "Tutorials" => Any["Static Von-Mises Truss" =>  "tutorials/StaticVonMisesTruss/staticVonMisesTruss.md",
-                           "Cantilever Beam" => "tutorials/CantileverBeam/cantileverBeam.md",
-                           "Uniaxial extension" => "tutorials/uniaxialExtension/uniaxialExtension.md"],
-        "Guide" => Any["Installation"     => "howtouse/install.md",
-                       "Creating Models"  => "howtouse/creatingModels.md"],
-        "Theory" => Any["Virtual mechanical work " => "theory/prinMechWork.md",
-                        "References"               => "theory/references.md"  ],
-        "About" => "about.md",
+      "index.md"
+      ]
+  )
+
+elseif outputFormat == "html"
+
+  makedocs(
+    sitename = "ONSAS.m",
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    pages = [
+      "Introduction" => "index.md"
     ],
     strict = false
-)
+  )
 
-deploydocs(
-    repo = "github.com/ONSAS/ONSAS_docs.git",
-    push_preview=true
-)
+  deploydocs(
+      repo = "github.com/ONSAS/ONSAS_docs.git",
+      push_preview=true
+  )
+
+end
+#
+#     pages = [
+#         "Home" => "index.md",
+#         "Tutorials" => Any["Static Von-Mises Truss" =>  "tutorials/StaticVonMisesTruss/staticVonMisesTruss.md",
+#                            "Cantilever Beam" => "tutorials/CantileverBeam/cantileverBeam.md",
+#                            "Uniaxial extension" => "tutorials/uniaxialExtension/uniaxialExtension.md"],
+#         "Guide" => Any["Installation"     => "howtouse/install.md",
+#                        "Creating Models"  => "howtouse/creatingModels.md"],
+#         "Theory" => Any["Virtual mechanical work " => "theory/prinMechWork.md",
+#                         "References"               => "theory/references.md"  ],
+#         "About" => "about.md",
+#     ],
+#     strict = false
+# )
+#
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,36 +1,13 @@
-# ONSAS: an Open Nonlinear Structural Analysis Solver
+# Introduction
 
-[![Join the chat at https://gitter.im/onsas_/community](https://badges.gitter.im/onsas_/community.svg)](https://gitter.im/onsas_/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+```@raw html
+<a href="https://gitter.im/onsas_/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge"><img src="https://badges.gitter.im/onsas_/community.svg" alt="Join the chat at https://gitter.im/onsas_/community">
+</a>
+```
 
 ## What is ONSAS?
 
-[ONSAS](https://github.com/ONSAS/ONSAS.m) is a set of solvers for static/dynamic and linear/non-linear analysis of structures. The first version was developed for educational purposes and published in a Structural Analysis [handbook](https://www.colibri.udelar.edu.uy/jspui/bitstream/20.500.12008/22106/1/Bazzano_P%c3%a9rezZerpa_Introducci%c3%b3n_al_An%c3%a1lisis_No_Lineal_de_Estructuras_2017.pdf). Currently different implementations and tools aimed for diverse applications are under development.
+[ONSAS](https://github.com/ONSAS/ONSAS.m) is a toolkit with implementations of different numerical methods for static/dynamic and linear/non-linear analysis of structures. The first version was developed for educational purposes and published in a Structural Analysis [handbook](https://www.colibri.udelar.edu.uy/jspui/bitstream/20.500.12008/22106/1/Bazzano_P%c3%a9rezZerpa_Introducci%c3%b3n_al_An%c3%a1lisis_No_Lineal_de_Estructuras_2017.pdf). Currently different implementations and tools aimed for diverse applications are under development.
 
-## What can ONSAS be used for?
 
-The current version allows to perform dynamic/static nonlinear analyses of beam/truss/solid 3D structures. A reduced list of features is listed at next:
-
-* **Elements** 2-node truss, 2-node Bernoulli frame, 4-node tetrahedron.
-* **Static analysis methods** Newton-Raphson Method and Cylindrical Arc-Length Method.
-* **Dynamic analysis methods** Newmark Method.
-* **Loads** nodal loads, time-history user-defined loading program.
-
-## Some examples
-
-### A wind turbine model
-
-![wind](https://github.com/ONSAS/ONSAS_docs/blob/master/gifs/wind.gif?raw=true)
-
-### A tower model
-
-![tower](https://github.com/ONSAS/ONSAS_docs/blob/master/gifs/tower.gif?raw=true)
-
-### A chain model
-
-![chain](https://user-images.githubusercontent.com/42485529/90902313-a6bf8d80-e3a2-11ea-8369-a9be639552f9.gif?raw=true)
-
-## Contributors and License
-
-The code is distributed under a [GNU-GPL 3.0 license](https://www.gnu.org/licenses/gpl-3.0.html).
-
-See the full list of contributors and their affiliations in the [Authors](@ref) section.
+ONSAS is an Open Nonlinear Structural Analysis Solver.
